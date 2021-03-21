d7db90f89a9643ebabc9b0f417d3e18afc00385b
mars-low <53299913+mars-low@users.noreply.github.com>
2021-03-21 21:13:50 +0100

    add ble support for heart rate monitor

--- app/src/main/java/com/example/medtracker/MainActivity.kt
+++ app/src/main/java/com/example/medtracker/MainActivity.kt
@@ -1,35 +1,15 @@
 package com.example.medtracker
 
 import Activity
-import History
 import HeartRateIndicator
+import History
 import Settings
-import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothManager
-import android.content.Intent
-import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.Toast
-import androidx.activity.result.ActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.activity.result.registerForActivityResult
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
-
-    private val requestBLE = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
-        if (result.resultCode == RESULT_OK) {
-            Log.i("DEBUG", "BLE enabled")
-        } else {
-            Log.i("DEBUG", "BLE disabled")
-        }
-
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -61,21 +41,4 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
             replace(R.id.flFragment, fragment)
             commit()
         }
-
-    fun toggleBLE(view: View) {
-        // Use this check to determine whether BLE is supported on the device.
-        // If not, we can selectively disable BLE-related features.
-        if(!packageManager.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
-            Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show()
-        } else {
-            val bluetoothManager = getSystemService(BluetoothManager::class.java)
-            val bluetoothAdapter: BluetoothAdapter? = bluetoothManager?.adapter
-
-            // Ensures Bluetooth is available on the device and it is enabled. If not,
-            // displays a dialog requesting user permission to enable Bluetooth.
-            if (bluetoothAdapter != null && !bluetoothAdapter.isEnabled) {
-                requestBLE.launch(Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE))
-            }
-        }
-    }
 }
---
+++ app/src/main/java/com/example/medtracker/ble/LeDeviceListAdapter.kt
@@ -0,0 +1,63 @@
+package com.example.medtracker.ble
+
+import android.bluetooth.BluetoothDevice
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.medtracker.R
+
+class LeDeviceListAdapter : RecyclerView.Adapter<LeDeviceListAdapter.ViewHolder>() {
+
+    // Provide a direct reference to each of the views within a data item
+    // Used to cache the views within the item layout for fast access
+    inner class ViewHolder(listItemView: View) : RecyclerView.ViewHolder(listItemView) {
+        // Your holder should contain and initialize a member variable
+        // for any view that will be set as you render a row
+        val deviceName: TextView = itemView.findViewById<TextView>(R.id.device_name)
+        val deviceAddress: TextView = itemView.findViewById<TextView>(R.id.device_address)
+    }
+
+    private val mLeDevices: ArrayList<BluetoothDevice> = ArrayList<BluetoothDevice>()
+
+    fun addDevice(device:  BluetoothDevice) {
+        if (!mLeDevices.contains(device)) {
+            mLeDevices.add(device)
+        }
+    }
+
+    fun getDevice(position: Int): BluetoothDevice {
+        return mLeDevices[position]
+    }
+
+    fun clear() {
+        mLeDevices.clear()
+    }
+
+    // Usually involves inflating a layout from XML and returning the holder
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LeDeviceListAdapter.ViewHolder {
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+        // Inflate the custom layout
+        val deviceView = inflater.inflate(R.layout.item_device, parent, false)
+        // Return a new holder instance
+        return ViewHolder(deviceView)
+    }
+
+    // Involves populating data into the item through holder
+    override fun onBindViewHolder(holder: LeDeviceListAdapter.ViewHolder, position: Int) {
+        // Get the data model based on position
+        val device: BluetoothDevice = mLeDevices[position]
+        // Set item views based on your views and data model
+        val deviceName = holder.deviceName
+        deviceName.text = device.name
+        val deviceAddress = holder.deviceAddress
+        deviceAddress.text = device.address
+    }
+
+    override fun getItemCount(): Int {
+        return mLeDevices.size
+    }
+}
