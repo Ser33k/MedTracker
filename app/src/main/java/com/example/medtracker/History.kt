0fcdaf342f64936204b56907e295116ddbf84e64
Mikolaj <mikus1001@gmail.com>
2021-03-22 20:13:59 +0100

    recyclerView simple styles

--- app/src/main/java/com/example/medtracker/History.kt
+++ app/src/main/java/com/example/medtracker/History.kt
@@ -14,40 +14,16 @@ class History:Fragment(R.layout.fragment_history) {
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-
-
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_history, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
         val rvHistory = view.findViewById(R.id.rvHistory) as RecyclerView
-        // Initialize contacts
-        historyItems = HistoryData.createRandomDataList(5)
-        // Create adapter passing in the sample user data
+        historyItems = HistoryData.createRandomDataList(15)
         val adapter = HistoryAdapter(historyItems)
-        // Attach the adapter to the recyclerview to populate items
         rvHistory.adapter = adapter
-        // Set layout manager to position the items
         rvHistory.layoutManager = LinearLayoutManager(context)
-        // That's all!
-
-
     }
-
-//    override fun onViewCreated(itemView: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(itemView, savedInstanceState)
-//                recyclerView = itemView.findViewById(R.id.recycler_view)
-//        recyclerView.apply {
-//            // set a LinearLayoutManager to handle Android
-//            // RecyclerView behavior
-//            layoutManager = LinearLayoutManager(activity)
-//            // set the custom adapter to the RecyclerView
-//            val list = arrayOf("dsad", "dsadsa", "dsads");
-//            adapter = HistoryAdapter(list)
-//        }
-//    }
 }
